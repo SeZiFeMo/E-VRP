c8d9cae499add5ad1f6ebc1caae3c8b8da9e7f42
TheJena <191685@studenti.unimore.it>
2017-06-06 17:36:48 +0200

    Merged into a single cache class shortest and greenest path from A to B.
    Added Path class to manage a sequence of nodes visited in sequence.
    Still to fix here and there 'energy' and 'time' attributes over edges.

--- graph.py
+++ graph.py
@@ -54,7 +54,7 @@ import utility
 utility.check_python_version()
 
 
-def add_elevation_properties_to_edges(graph):
+def add_energy_cost_to_edges(graph):
     """Add to edge properties slope and rise."""
     altitude = utility.CLI.args().altitude
     for node1, adjacency_dict in graph.adjacency_iter():
@@ -64,8 +64,11 @@ def add_elevation_properties_to_edges(graph):
             x2, y2 = node2  # to
             run = math.hypot(x2 - x1, y2 - y1)
             slope = math.atan2(rise, run)  # in radians
-            graph.add_edge(node1, node2, attr_dict={'rise': rise,
-                                                    'slope': slope})
+            energy = rise  # TODO need some physics here
+            graph.add_edge(node1, node2, attr_dict={'energy': energy,
+                                                    'rise': rise,
+                                                    'slope': slope,
+                                                    'time': 0}) # TODO
 
 
 def check_problem_solvability(graph):
@@ -192,8 +195,8 @@ def export_problem_to_directory():
 
 def get_abstract_graph(graph):
     """Create a copy of the graph without unnecessary attributes."""
-    necessary_edge_attr = ('length', 'oneway', 'osm_id',
-                           'rise', 'slope', 'speed')
+    necessary_edge_attr = ('energy', 'length', 'oneway', 'osm_id', 'rise',
+                           'slope', 'speed', 'time')
     altitude = utility.CLI.args().altitude
     ret = nx.DiGraph()
 
@@ -207,6 +210,7 @@ def get_abstract_graph(graph):
                 continue
 
             attr = dict()
+            attr['energy'] = data['energy']
             attr['length'] = data['length']
             attr['osm_id'] = data['osm_id']
             attr['rise'] = data['rise']
@@ -217,9 +221,11 @@ def get_abstract_graph(graph):
                 attr['speed'] = data['maxspeed']
             else:
                 attr['speed'] = 50  # default value if no speed available
+            # TODO add time !!
 
             ret.add_edge(node1, node2, attr_dict=attr)
             if not data['oneway']:
+                attr['energy'] *= -1  # TODO need some physics here
                 attr['rise'] *= -1
                 attr['slope'] *= -1
                 ret.add_edge(node2, node1, attr_dict=attr)
@@ -319,98 +325,129 @@ def print_edge_properties(graph, fclass_whitelist=None, tag_blacklist=None):
                         print('{}: {}'.format(tag, data[tag]))
 
 
-class CacheBellmanFordPaths(object):
-    """Wrap some useful methods around bellman-ford's shortest paths."""
+class CachePaths(object):
+    """Cache of shortest and most energy-efficient routes."""
 
-    __bellmanford = None
+    __cache = None
+    """List of tuples:
+       ( (src_lat, src_lon), (dst_lat, dst_lon), greenest_path, shortest_path )
+    """
 
-    def __init__(self, graph, weight='rise',
-                 type_whitelist=('depot', 'customer', 'station')):
-        """Create cache of shortest paths over a graph."""
-        self.__bellmanford = dict()
+    def __add(self, graph, src, dest, greenest, shortest):
+        """Append to cache two new paths between src and dest."""
+        if src == dest:
+            return
+        record = (src, dest, Path(graph, greenest), Path(graph, shortest))
+        for index, tup in enumerate(self.__cache):
+            # update record if already existing
+            if tup[0:2] == (src, dest):
+                self.__cache[index] = record
+                return
+        else:
+            self.__cache.append(record)
 
-        # for each depot, customer, station
+    def __init__(self, graph, type_whitelist=('depot', 'customer', 'station')):
+        """Compute shortest and most efficient path."""
+        self.__cache = list()
+
+        # from each depot, customer, station ...
         for src_node, src_data in graph.nodes_iter(data=True):
             if src_data['type'] in type_whitelist:
-                # get lenghts and paths to all other nodes
-                predecessors, lengths = nx.bellman_ford(graph, src_node,
-                                                        weight=weight)
-                # for each depot, customer, destination in lenghts
+
+                # get shortest paths starting from src_node
+                shortest_path = nx.single_source_dijkstra_path(graph,
+                                                               src_node,
+                                                               weight='lenght')
+
+                # get most energy-efficient paths from src_node
+                greenest_t = nx.bellman_ford(graph, src_node, weight='energy')
+                g_pred, g_energy = greenest_t
+
+                # ... to other depot, customer, destination
                 for dest_node, dest_data in graph.nodes_iter(data=True):
                     if dest_data['type'] in type_whitelist \
                        and dest_node != src_node \
-                       and dest_node in lengths:
+                       and dest_node in shortest_path \
+                       and dest_node in g_energy:
                         # unroll the path from predecessors dictionary
-                        path = list()
+                        greenest_path = list()
                         node_to_add = dest_node
-                        while predecessors[node_to_add] != src_node:
-                            path.append(node_to_add)
-                            node_to_add = predecessors[node_to_add]
-                        path.append(src_node)
-                        path = list(reversed(path))
-                        # cache the lenght and the path in a dictionary
-                        if src_node not in self.__bellmanford:
-                            self.__bellmanford[src_node] = dict()
-                        d = {'length': lengths[dest_node], 'path': path}
-                        self.__bellmanford[src_node][dest_node] = d
+                        while g_pred[node_to_add] != src_node:
+                            greenest_path.append(node_to_add)
+                            node_to_add = g_pred[node_to_add]
+                        greenest_path.append(src_node)
+                        greenest_path = list(reversed(greenest_path))
 
-    def get_length(self, src, dest):
-        """Raises NetworkXNoPath on cache miss."""
-        if src in self.__bellmanford:
-            if dest in self.__bellmanford[src]:
-                return self.__bellmanford[src][dest]['length']
-        raise nx.exception.NetworkXNoPath('Length not found in cache')
+                        self.__add(graph, src_node, dest_node,
+                                   greenest_path, shortest_path)
 
-    def get_path(self, src, dest):
-        """Raises NetworkXNoPath on cache miss."""
-        if src in self.__bellmanford:
-            if dest in self.__bellmanford[src]:
-                return self.__bellmanford[src][dest]['path']
-        raise nx.exception.NetworkXNoPath('Path not found in cache')
+    def greenest(self, src, dest):
+        """Return greenest Path between src and dest."""
+        for source, destination, greenest, shortest in self.__cache:
+            if src == source and dest == destination:
+                return greenest
+        raise nx.exception.NetworkxNoPath('No greenest path found between '
+                                          '{} and {}'.format(src, dest))
+
+    def shortest(self, src, dest):
+        """Return shortest Path between src and dest."""
+        for source, destination, greenest, shortest in self.__cache:
+            if src == source and dest == destination:
+                return shortest
+        raise nx.exception.NetworkxNoPath('No greenest path found between '
+                                          '{} and {}'.format(src, dest))
+
+    def source_iterator(self, src):
+        """Return iterator over cached records starting from src."""
 
 
-class CacheDijkstraPaths(object):
-    """Wrap some useful methods around dijkstra's shortest paths."""
+class Path(object):
 
-    __dijkstra = None
+    __graph = None
+    __node_list = None
 
-    def __init__(self, graph, weight='length',
-                 type_whitelist=('depot', 'customer', 'station')):
-        """Create cache of shortest paths over a graph."""
-        self.__dijkstra = dict()
+    def __init__(self, graph, coor_list):
+        """Initialize a path from a list of node coordinates."""
+        self.__graph = graph
+        for lat, lon in coor_list:
+            self.append({'latitude': lat,
+                         'longitude': lon,
+                         'type': graph.node[(lat, lon)]['type']})
 
-        # for each depot, customer, station
-        for src_node, src_data in graph.nodes_iter(data=True):
-            if src_data['type'] in type_whitelist:
-                # get lenghts and paths to all other nodes
-                lengths, paths = nx.single_source_dijkstra(graph, src_node,
-                                                           weight=weight)
-                # for each depot, customer, destination in lenghts and paths
-                for dest_node, dest_data in graph.nodes_iter(data=True):
-                    if dest_data['type'] in type_whitelist \
-                       and dest_node != src_node \
-                       and dest_node in lengths \
-                       and dest_node in paths:
-                        # cache the lenght and the path in a dictionary
-                        if src_node not in self.__dijkstra:
-                            self.__dijkstra[src_node] = dict()
-                        d = {'length': lengths[dest_node],
-                             'path': paths[dest_node]}
-                        self.__dijkstra[src_node][dest_node] = d
+    def __repr__(self):
+        return repr(self.__node_list)
 
-    def get_length(self, src, dest):
-        """Raises NetworkXNoPath on cache miss."""
-        if src in self.__dijkstra:
-            if dest in self.__dijkstra[src]:
-                return self.__dijkstra[src][dest]['length']
-        raise nx.exception.NetworkXNoPath('Length not found in cache')
+    def __str__(self):
+        return str(self.__node_list)
 
-    def get_path(self, src, dest):
-        """Raises NetworkXNoPath on cache miss."""
-        if src in self.__dijkstra:
-            if dest in self.__dijkstra[src]:
-                return self.__dijkstra[src][dest]['path']
-        raise nx.exception.NetworkXNoPath('Path not found in cache')
+    def append(self, node):
+        """Insert node in last position of the node list."""
+        if self.__node_list is None:
+            self.__node_list = list()
+        self.__node_list.append(node)
+
+    def remove(self, node):
+        """Remove from node list the specified node."""
+        raise Exception('Not yet implemented')
+
+    def substitute(self, old_node, new_node):
+        """Replace the old node with the new one."""
+        raise Exception('Not yet implemented')
+
+    def energy(self):
+        """Sum of the energies of each edge between the nodes in the list."""
+        return 55
+        raise Exception('Not yet implemented')
+
+    def length(self):
+        """Sum of the lengths of each edge between the nodes in the list."""
+        return 44
+        raise Exception('Not yet implemented')
+
+    def time(self):
+        """Sum of the times of each edge between the nodes in the list."""
+        return 33
+        raise Exception('Not yet implemented')
 
 
 # ----------------------------------- MAIN ---------------------------------- #
@@ -431,22 +468,29 @@ if utility.CLI.args().workspace is None:
 check_workspace()  # <-- it exits if workspace is not compliant
 
 g = nx.read_shp(path=utility.CLI.args().workspace, simplify=True)
-add_elevation_properties_to_edges(g)
+add_energy_cost_to_edges(g)
 label_nodes(g)  # <-- it exits if problem file is not applicable to graph
 check_problem_solvability(g)
 abstract_g = get_abstract_graph(g)
 
 temp_graph = nx.DiGraph()
-cache = CacheBellmanFordPaths(abstract_g)
+cache = CachePaths(abstract_g)
 for coor1, data1 in abstract_g.nodes_iter(data=True):
     if data1['type'] in ('depot', 'customer', 'station'):
         for coor2, data2 in abstract_g.nodes_iter(data=True):
             if data2['type'] in ('depot', 'customer', 'station'):
                 if coor1 != coor2:
                     temp_graph.add_edge(coor1, coor2,
-                                        {'cost': cache.get_length(coor1,
-                                                                  coor2),
-                                         'path': cache.get_path(coor1,
-                                                                coor2)})
+                                        {'shortest_length':
+                                         cache.shortest(coor1, coor2).length(),
+                                         'shortest_path':
+                                         str(cache.shortest(coor1, coor2)),
+
+                                         'greenest_energy':
+                                         cache.greenest(coor1, coor2).energy(),
+                                         'greenest_path':
+                                         repr(cache.shortest(coor1, coor2)),
+
+                                         })
 print('\n' + '#' * 80)
 print_edge_properties(temp_graph)

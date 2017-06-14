c0eb6452273608a2350dfabdf302b42c381ff00f
TheJena <191685@studenti.unimore.it>
2017-06-15 01:25:43 +0200

    Fixed unittest patch to be compliant with previous commit changes.
    Few things to remember:
    - Graph(from_DiGraph= $existing_graph ) will automatically create a new
      graph by passing the existing graph to _get_abstract_graph() method;
      thus latitude and longitude attributes will be added to vertexes while
      rise, energy, slope and time attributes will be added to edges. Pay
      attention that $existing_graph coordinates format is OpenStreetMap one
      (longitude, latitude); while returned graph coordinates are in
      (latitude, longitude) format;
    - if CachePaths does not include type '' in whitelist those nodes can
      not be added to routes (in fact routes should be within nodes of
      interest); so doing that would raise a ValueError('node not in cache')
      exception;
    - routes always start from depot, so appending the depot node to an
      empty route does not make any sense, since there are no self-edges
      between nodes (like depot <--edge--> depot); so doing that would raise
      a nx.exception.NetworkXNoPath('no path found between src and dest')
      exception;
    - route constructor does not take an argument "initial list of nodes";
    - calling route.substitute( node1, node2 ) will not swap the passed
      nodes; it will remove the first one and add the second one in the
      place of the removed one (note that node2 must not be in the route)
      example:    route_a:               (depot, cust1, stat3, cust4, ...)
                  nodes not in route_a:   cust5, ...
    
                  route_a.substitute(cust1, cust5)
    
                  route_a:               (depot, cust5, stat3, cust4, ...)
                  nodes not in route_a:   cust1, ...

--- tests/context.py
+++ tests/context.py
@@ -2,7 +2,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import solution
-import heuristic
 import graph
-
+import heuristic
+import solution
--- tests/test_solution.py
+++ tests/test_solution.py
@@ -31,14 +31,15 @@ __license__ = "GPL3"
 import unittest
 import networkx as nx
 
+from context import graph
 from context import solution
 
 
 class test_solution_class(unittest.TestCase):
 
     def setUp(self):
-        graph = nx.DiGraph()
-        self.sol = solution.Solution(graph)
+        stub_graph = nx.DiGraph()
+        self.sol = solution.Solution(stub_graph)
 
     def test_is_feasible(self):
         self.assertTrue(self.sol.is_feasible())
@@ -47,45 +48,85 @@ class test_solution_class(unittest.TestCase):
 class test_route_class(unittest.TestCase):
 
     def setUp(self):
-        self.graph = nx.DiGraph()
-        self.graph.add_node((16, 48), {'altitude': 1, 'type': 'depot',
-                                       'longitude': 16, 'latitude': 48})
-        self.graph.add_node((17, 49), {'altitude': 2, 'type': 'station',
-                                       'longitude': 17, 'latitude': 49})
-        self.graph.add_node((18, 50), {'altitude': 3, 'type': 'customer',
-                                       'longitude': 18, 'latitude': 50})
-        self.graph.add_node((19, 51), {'altitude': 4, 'type': '',
-                                       'longitude': 19, 'latitude': 51})
-        self.route = solution.Route(self.graph)
+        alt_lab = 'ASTGTM2_de'
+        stub_graph = nx.DiGraph(name=graph.Graph._osm_name)
+        # coordinates are (longitude, latitude)
+        stub_graph.add_node((16, 48), {alt_lab: 0, 'type': 'depot'})
+        stub_graph.add_node((17, 49), {alt_lab: 2, 'type': 'station'})
+        stub_graph.add_node((18, 50), {alt_lab: 0, 'type': 'customer'})
+        stub_graph.add_node((19, 51), {alt_lab: 4, 'type': ''})
+        stub_graph.add_edge((16, 48), (17, 49), {'osm_id': 211937039,
+                                                 'length': 20,
+                                                 # rise = 2 m
+                                                 'speed': 20,
+                                                 # energy = 8519818.68 Joule
+                                                 # slope = 0.09966865249116 rad
+                                                 # time = 0.06 min
+                                                 'oneway': False})
+        stub_graph.add_edge((17, 49), (18, 50), {'osm_id': 165738417,
+                                                 'length': 57,
+                                                 # rise = -2 m
+                                                 'speed': 20,
+                                                 # energy = 24229479.12 Joule
+                                                 # slope = -0.0350733305332 rad
+                                                 # time = 0.170999999999999 min
+                                                 'oneway': False})
+        stub_graph.add_edge((18, 50), (16, 48), {'osm_id': 33170597,
+                                                 'length': 54,
+                                                 # rise = 0 m
+                                                 'speed': 45,
+                                                 # energy = 22939200.0 Joule
+                                                 # slope = 0.0 rad
+                                                 # time = 0.072
+                                                 'oneway': False})
+        stub_graph.add_edge((18, 50), (19, 51), {'osm_id': 12345678,
+                                                 'length': 10,
+                                                 # rise = 4 m
+                                                 'speed': 50,
+                                                 # energy = 4295637.36 Joule
+                                                 # slope = 0.38050637711236 rad
+                                                 # time = 0.012 min
+                                                 'oneway': False})
+
+        # self.graph = Graph._get_abstract_graph(stub_graph)  # it is be
+        self.graph = graph.Graph(from_DiGraph=stub_graph)     # the same
+        # now self.graph has different attributes from stub_graph
+        # nodes have: altitude, type, latitude, longitude
+        # edges have: osm_id, length, rise, speed, energy, slope, time
+        # self.graph nodes have also inverted coordinates (lat, lon)
+
+        self.cache = graph.CachePaths(self.graph)
+        self.route = solution.Route(self.cache, greenest=True)
 
     def test_create_route_with_nodes_list(self):
-        route = solution.Route(self.graph, [(16, 48), (19, 51)])
+        # constructor does not get a list of nodes
+        route = solution.Route(self.cache, greenest=True)
+        # node can be add after with a for loop
+        for coor in [(49, 17, 'station'), (48, 16, 'depot')]:
+            route.append(coor)
 
     def test_is_feasible(self):
-        path = []
-        battery = []
-        self.assertTrue(self.route.is_feasible(path, battery))
+        self.assertTrue(self.route.is_feasible(paths=list(), batteries=list()))
 
     def test_append_node(self):
-        node = (16, 48, 'depot')
+        node = (49, 17, 'station')
         self.route.append(node)
-        self.assertEqual(len(self.route._path._nodes),
-                         len(self.route._battery))
+        self.assertEqual(len(self.route._paths), len(self.route._batteries))
 
     def test_remove_node(self):
-        node = (16, 48, 'depot')
+        node = (50, 18, 'customer')
         self.route.append(node)
         self.route.remove(node)
-        self.assertEqual(len(self.route._path._nodes),
-                         len(self.route._battery))
+        self.assertEqual(len(self.route._paths), len(self.route._batteries))
 
     def test_substitute_node(self):
-        route = solution.Route(self.graph, [(16, 48), (17, 49)], [3, 3])
-        node1 = (16, 48, 'depot')
-        node2 = (17, 49, 'station')
+        route = solution.Route(self.cache, greenest=True)
+        for coor in [(49, 17, 'station'), (48, 16, 'depot')]:
+            route.append(coor)
+        node1 = (49, 17, 'station')
+        node2 = (50, 18, 'customer')
         route.substitute(node1, node2)
-        self.assertEqual(len(self.route._path._nodes),
-                         len(self.route._battery))
+        self.assertEqual(len(self.route._paths), len(self.route._batteries))
 
     def test_raise(self):
         self.assertRaises(solution.UnfeasibleRouteException, raiser)
@@ -93,6 +134,7 @@ class test_route_class(unittest.TestCase):
 
 def raiser():
     raise solution.UnfeasibleRouteException('Exception tester')
+
 
 if __name__ == '__main__':
     unittest.main(failfast=False)
